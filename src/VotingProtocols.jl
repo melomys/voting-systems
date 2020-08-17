c5b596e1dec8545b69e38d734feb0b78007481fe
melomys <felix.matuschka@rwth-aachen.de>
2020-08-18 00:23:47 +0200

    fixing module

--- src/VotingProtocols.jl
+++ src/VotingProtocols.jl
@@ -9,8 +9,7 @@ using RCall
 include("user_creation.jl")
 
 include("models/upvote_system.jl")
-include("models/up_and_downvotesystem.jl")
-include("models/random_model.jl")
+include("models/up_and_downvote_system.jl")
 
 include("model_factory.jl")
 include("metric.jl")
@@ -25,7 +24,7 @@ include("export_r.jl")
 
 export upvote_system, up_and_downvote_system
 export export_data
-export default_evaluation_functions, default_model_properties,
+export default_evaluation_functions, default_model_properties
 export dcg, ndcg, spearman, gini, @top_k_gini, @area_under, post_with_no_views, quality_sum, @model_df_column, post_views, vote_count
 export metric_hacker_news, metric_reddit_hot, metric_activation, metric_view
 export vote_difference, vote_partition, vote_wilson
